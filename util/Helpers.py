3e35cfa4f4cd489fc9469c1048399e65105323b9
Mindaugas Straksys <gamerx14x@gmail.com>
2025-09-23 13:23:02 +0300

    refactor: moved util functions to util folder and added more of them

--- util/Helpers.py
+++ util/Helpers.py
@@ -1,3 +1,9 @@
+# Utils
+def is_even(x):
+	return x % 2 == 0
+def is_odd(x):
+	return x % 2 == 1
+	
 def goto(x, y):
     yDist = get_pos_y() - y  # Positive if drone is north of the target space
     xDist = get_pos_x() - x  # Positive if drone is east of the target space
@@ -12,6 +18,16 @@ def goto(x, y):
             move(East)
         else:
             move(West)
+            
+def backtrack(route, x, y):
+    if x > route[0]:
+        move(West)
+    elif x < route[0]:
+        move(East)
+    elif y > route[1]:
+        move(South)
+    elif y < route[1]:
+        move(North)
 
 
 def fieldGrid(size, element):
@@ -22,3 +38,25 @@ def fieldGrid(size, element):
             yArr.append(element)
         xArr.append(yArr)
     return xArr
+
+
+    
+def moveToNextTile(dir):
+	bX = get_world_size() #Farm width (x Boundary)
+	bY = bX #Farm height (y Boundary)
+	cX = get_pos_x() #Current X position
+	cY = get_pos_y() #Current Y position
+	if (dir==East):
+		if (cX+1==bX):
+			dir=North
+	elif (dir==West):
+		if (cX==0):
+			dir=North
+	elif (dir==North):
+		if (cX+1==bX):
+			dir=West
+		elif (cX==0):
+			dir=East
+			
+	move(dir)
+	return dir

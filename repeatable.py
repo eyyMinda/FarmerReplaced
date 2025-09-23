28f22938703e09aad58e78fca85b28ebc8503d8d
Mindaugas Straksys <gamerx14x@gmail.com>
2025-09-23 13:24:48 +0300

    refactor: seeds were removed

--- repeatable.py
+++ repeatable.py
@@ -1,43 +1,7 @@
-def Till():
-    if not is_over(Grounds.Soil):
-        till()
+# TODO: add remade checkSeeds function by checking the cost of current selected entity
+# and if there is enough of it for at least a whole field. Otherwise run replant to farm more
 
-
-def useFertilizer():
-    trade(Items.Fertilizer)
-    use_item(Items.Fertilizer)
-
-
-def checkSeeds(size, entity, buySeeds):
-
-    entityList = [Entities.Carrots, Entities.Pumpkin]
-    seedsList = [Items.Carrot_Seed, Items.Pumpkin_Seed]
-    inventory = get_inventory()
-
-    seedCost = 1
-    minCost = 50
-    for i in range(len(entityList)):
-        if entity == entityList[i]:
-            seedCost = get_cost(seedsList[i])
-            break
-
-    for slot in inventory:
-        item = slot[0]
-        amount = slot[1]
-
-        if entity == entityList[0]:  # For planting carrots
-            if item == Items.Hay and amount <= seedCost[1][1] * minCost:
-                runLoop(20, [size, Entities.Grass])
-            elif item == Items.Wood and amount <= seedCost[0][1] * minCost:
-                runLoop(20, [size, Entities.Bush])
-
-        elif entity == entityList[1]:  # For planting pumpkins
-            if item == Items.Carrot and amount <= seedCost[0][1] * minCost:
-                runLoop(50, [size, Entities.Carrots])
-
-    trade(seedsList[i], buySeeds)
-
-
-def runLoop(times, list):
+def runLoop(times, info):
     for i in range(times):
-        replant(list[0], list[1], 0.8, True)
+        quick_print("Loop for", info[1], [i, times])
+        replant(info[0], info[1], 0.8, True)
